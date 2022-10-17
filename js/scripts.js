188653a80cfd6b1f95274b53caca8e9e3c21cf33
Phil Kendall <prkkendall@gmail.com>
2022-10-17 16:07:40 -0700

    add sub, mult, and div function to calculator and output user input onto one window alert.
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,27 @@ function add (number1, number2){
   return number1 + number2;
 }
 
+function sub (number1, number2){
+  return number1 - number2;
+}
+
+function mult (number1, number2){
+  return number1 * number2;
+}
+
+function div (number1, number2){
+  return number1 / number2;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+// window.alert(add(number1, number2), sub(number1, number2));
+window.alert( add(number1, number2) + ' ' + sub(number1, number2) + ' ' + mult(number1, number2) + ' ' + div(number1, number2));
+// window.alert(sub(number1, number2));
+// window.alert(mult(number1, number2));
+// window.alert(div(number1, number2));
 
 // const additionResult = add(10, 5);
 // window.alert(additionResult);
