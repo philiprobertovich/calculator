7beeb9b7536d1cb1636af728a214778b6b3ada1d
Phil Kendall <prkkendall@gmail.com>
2022-10-17 15:38:41 -0700

    add an adition function to a Javascript file
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,22 @@ function saySomething(whatToSay) {
   window.alert(whatToSay);
 }
 
+// business logic
 function add (number1, number2){
   return number1 + number2;
 }
 
-saySomething("hi");
+// user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-const result = add(3, 5);
-const outputText = "The sum is " + result + ".";
-saySomething(outputText);
+window.alert(add(number1, number2));
+
+// const additionResult = add(10, 5);
+// window.alert(additionResult);
+// saySomething("hi");
+
+// const result = add(3, 5);
+// const outputText = "The sum is " + result + ".";
+// saySomething(outputText);
 // saySomething("The sum is " + add(3, 5) + ".");
